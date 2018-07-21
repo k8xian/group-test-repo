e90da0ff5e090c0a7ab41db0c5ebb62eee4b33f1
k8xian <kate@innovationsforlearning.org>
2018-07-21 14:21:24 -0500

    fixing everything I broke :(

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,6 +50,7 @@ $(document).ready(function () {
   })
 
 
+
   //map styling
   function initMap() {
     // Styles a map in night mode.
@@ -140,25 +141,7 @@ $(document).ready(function () {
       ]
     });
   };
+  initMap();
 
-
-//beginning of the map API business
-var apiKey = "AIzaSyBLYh-dwOzaEbsAMeBCP2TQtvvFqFioKSg";
-var query = "happyhour";
-
-  var queryURL = "https://www.googleapis.com/customsearch/v1?key=" 
-                + apiKey 
-                + "&cx=017576662512468239146:omuauf_lfve&"
-                + "q="
-                + query;
-  
-console.log(queryURL);
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
 
 });
